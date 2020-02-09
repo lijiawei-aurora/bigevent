37fe8ebe863a85d7d0d36ffaebabdfa1a5e6d398
iljiawei <1030855421@qq.com>
2020-02-09 09:28:08 +0800

    函数-

--- web_back/js/util/category.js
+++ web_back/js/util/category.js
@@ -1,6 +1,8 @@
 // 把所有与文章分类的操作写在这里
+// 改为箭头函数
 let category = {
-    get: function() {
-        return $.get(APILIST.category_get)
-    }
-}
+    get: () => $.get(APILIST.category_get)
+        // get: function() {
+        //     return $.get(APILIST.category_get)
+        // }
+};
--- web_back/js/util/user.js
+++ web_back/js/util/user.js
@@ -1,6 +1,5 @@
 // 创建一个user对象
 var user = {
-
     // 用户登录
 
     // $.post(url,数据).then(回调函数)
@@ -10,23 +9,19 @@ var user = {
     // }).then(function(res) {
     //     console.log(res);
     // });
-    // login为方法   
+    // login为方法
     // 匿名函数  var 变量名=function(){}
-    login: function(name, password) {
+    // 函数均改为箭头函数
+    login: (name, password) => {
         // APILIST.user_login 为接口  config.js提供
         return $.post(APILIST.user_login, {
-            'user_name': name,
-            'password': password
-        })
+            user_name: name,
+            password: password
+        });
     },
 
     // 用户退出  函数没有参数，接口也没有数据
-    logout: function() {
-        return $.post(APILIST.user_logout)
-    },
+    logout: () => $.post(APILIST.user_logout),
     // 获取信息
-    getInfo: function() {
-        return $.get(APILIST.user_getInfo)
-    }
-
-}
+    getInfo: () => $.get(APILIST.user_getInfo)
+};
