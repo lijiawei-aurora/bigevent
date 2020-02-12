f79bc99dbc8a732a2a7150d991872ad71d5741e7
iljiawei <1030855421@qq.com>
2020-02-12 20:50:39 +0800

    完成发布文章功能

--- web_back/js/util/article.js
+++ web_back/js/util/article.js
@@ -7,5 +7,16 @@ let article = {
     // 此行代码功能同上，箭头函数，属性名和变量名相同所以可省略属性名
     // type 为文章类别
     get: (page, state, type) => $.get(APILIST.article_get, { page, state, type }),
-    del: id => $.get(APILIST.article_del, { id })
+    del: id => $.get(APILIST.article_del, { id }),
+    add: fd => {
+        return $.ajax({
+            url: APILIST.article_publish,
+            type: "post",
+            data: fd,
+            // 不允许处理数据
+            processData: false,
+            // 不设置请求头
+            contentType: false
+        });
+    }
 };
