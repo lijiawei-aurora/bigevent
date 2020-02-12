8ff6251f46c6140ed34eaf0a10bb7596666b5899
iljiawei <1030855421@qq.com>
2020-02-12 16:51:43 +0800

    文章分页功能和筛选功能

--- web_back/js/util/article.js
+++ web_back/js/util/article.js
@@ -1,5 +1,10 @@
 // 所有文章相关的操作
 let article = {
     // 获取文章
-    get: () => $.get(APILIST.article_get)
+    // get: function (page) {
+    //     return $.get(APILIST.article_get, {"page": page })
+    // }
+    // 此行代码功能同上，箭头函数，属性名和变量名相同所以可省略属性名
+    // type 为文章类别
+    get: (page, state, type) => $.get(APILIST.article_get, { page, state, type })
 };
